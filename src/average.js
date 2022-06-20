04faa8c5c3619400dc94f195367227e9ce27348e
Luiz Filipe <luizfilipelgs@gmail.com>
2022-06-20 17:10:49 -0300

    realização 1-3

--- src/average.js
+++ src/average.js
@@ -12,6 +12,25 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let sum = 0;
+  let media = 0;
+
+  for (let index = 0; index < array.length; index += 1) {
+    sum += array[index];
+    if (typeof (array[index]) !== 'number') { 
+    return undefined;
+    }
+  }
+ 
+  if (array.length === 0) {
+    return undefined;
+  }  
+  
+  media = Math.round(sum / array.length);
+  return media;
+};
+
+console.log(average([2, 2]));
 
 module.exports = average;
