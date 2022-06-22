93bff4572935671d309920f2381e8cb8b5bce90a
Luiz Filipe <luizfilipelgs@gmail.com>
2022-06-21 23:11:41 -0300

    Correção Linter objPlayground

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -34,14 +34,12 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
 */
 
-const calculator = (number1, number2) => {
-  return {
+const calculator = (number1, number2) => ({
     sum: (Math.floor((number1 + number2), 1)),
     mult: (Math.floor((number1 * number2), 1)),
     div: (Math.floor((number1 / number2), 1)),
     sub: (Math.floor((number1 - number2), 1)),
-  };
-};
+  });
 
 const arrayGenerator = (type, object) => {
   if (type === 'keys') {
